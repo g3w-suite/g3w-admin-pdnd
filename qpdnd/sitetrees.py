7282507018c7d893e75fc3aa387ad2632d7a8370
wlorenzetti <lorenzett@gis3w.it>
2024-09-13 08:50:56 +0200

    Typo

--- qpdnd/sitetrees.py
+++ qpdnd/sitetrees.py
@@ -7,7 +7,7 @@ sitetrees = (
   G3Wtree('qpdnd', title='PDND', module='qpdnd', items=[
       # Then define items and their children with `item` function.
       item('PDND (OGC API)', '#', type_header=True),
-      item('Servizi', '#', icon_css_class='fa fa-world', children=[
+      item('Servizi', '#', icon_css_class='fa fa-globe', children=[
           item('Aggiungi servizio', 'qpdnd-project-add', url_as_pattern=True, icon_css_class='fa fa-plus',
                access_by_perms=['qpdnd.add_qpdndproject']),
           item('Lista servizi', 'qpdnd-project-list', url_as_pattern=True, icon_css_class='fa fa-list'),
@@ -19,7 +19,7 @@ sitetrees = (
   G3Wtree('qpdnd_en', title='PDND', module='qpdnd', items=[
       # Then define items and their children with `item` function.
       item('PDND (OGC API)', '#', type_header=True),
-      item('Services', '#', icon_css_class='fa fa-world', children=[
+      item('Services', '#', icon_css_class='fa fa-globe', children=[
           item('Add reporting', 'qpdnd-project-add', url_as_pattern=True, icon_css_class='fa fa-plus',
                access_by_perms=['qpdnd.add_qpdndproject']),
           item('Services list', 'qpdnd-project-list', url_as_pattern=True, icon_css_class='fa fa-list'),
