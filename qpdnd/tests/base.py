6b094ebbd1257d18c02d45d6998a2948eaa698e2
wlorenzetti <lorenzett@gis3w.it>
2024-09-13 07:59:56 +0200

    Add tests for form

---
+++ qpdnd/tests/base.py
@@ -0,0 +1,81 @@
+# coding=utf-8
+""""
+    Base testing module
+.. note:: This program is free software; you can redistribute it and/or modify
+    it under the terms of the Mozilla Public License 2.0.
+
+"""
+
+__author__ = 'lorenzetti@gis3w.it'
+__date__ = '2023-12-28'
+__copyright__ = 'Copyright 2015 - 2023, Gis3w'
+__license__ = 'MPL 2.0'
+
+from django.test import TestCase, override_settings
+from django.core.files import File
+from core.models import G3WSpatialRefSys, Group as CoreGroup
+from usersmanage.tests.utils import setup_testing_user
+from qdjango.utils.data import QgisProject
+import os
+
+
+CURRENT_PATH = os.path.dirname(__file__)
+TEST_BASE_PATH = 'data/'
+DATASOURCE_PATH = '{}/{}project_data'.format(CURRENT_PATH, TEST_BASE_PATH)
+
+QGS_PROJECT_FILE = 'projects/test_ogc_api.qgs'
+QGS_PROJECT_FILE_NO_WFS_ACTIVED = 'projects/test_ogc_api_no_wfs_actived.qgs'
+
+
+
+@override_settings(
+    CACHES={
+        'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'some',
+        }
+    },
+    DATASOURCE_PATH=DATASOURCE_PATH,
+    LANGUAGE_CODE='en',
+    LANGUAGES = (
+        ('en', 'English'),
+    ),
+)
+class TestQPDNDBase(TestCase):
+
+    fixtures = ['BaseLayer.json',
+                'G3WMapControls.json',
+                'G3WSpatialRefSys.json',
+                'G3WGeneralDataSuite.json',
+                'license.json'
+                ]
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+
+        setup_testing_user(cls)
+
+        cls.qgis_file = os.path.join(CURRENT_PATH, TEST_BASE_PATH, QGS_PROJECT_FILE)
+        cls.qgis_file_no_wfs_actived = os.path.join(CURRENT_PATH, TEST_BASE_PATH, QGS_PROJECT_FILE_NO_WFS_ACTIVED)
+
+    @classmethod
+    def setUp(cls):
+
+        # Main project group
+        cls.project_group = CoreGroup(name='QPDND', title='QPDND', header_logo_img='',
+                                      srid=G3WSpatialRefSys.objects.get(auth_srid=4326))
+        cls.project_group.save()
+
+        # Add projects to DB
+        qgis_file = File(open(cls.qgis_file, 'r'))
+        cls.project = QgisProject(qgis_file)
+        cls.project.title = 'OGC API test project'
+        cls.project.group = cls.project_group
+        cls.project.save()
+
+        qgis_file = File(open(cls.qgis_file_no_wfs_actived, 'r'))
+        cls.project_no_wfs = QgisProject(qgis_file)
+        cls.project_no_wfs.title = 'OGC API test project NO WFS ACTIVED'
+        cls.project_no_wfs.group = cls.project_group
+        cls.project_no_wfs.save()
---
+++ qpdnd/tests/test_forms.py
@@ -0,0 +1,88 @@
+# coding=utf-8
+"""" Test for forms
+.. note:: This program is free software; you can redistribute it and/or modify
+    it under the terms of the Mozilla Public License 2.0.
+
+"""
+
+__author__ = 'lorenzetti@gis3w.it'
+__date__ = '2024-08-16'
+__copyright__ = 'Copyright 2015 - 2024, Gis3w'
+__license__ = 'MPL 2.0'
+
+from django.test.client import RequestFactory
+from .base import TestQPDNDBase
+from qpdnd.forms import QPDNDProjectForm
+from qpdnd.models import QPDNDProject
+import copy
+
+
+class TestQPDNDForms(TestQPDNDBase):
+    """
+    Test irpi_up forms module
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.request = RequestFactory()
+
+    def test_projects(self):
+
+
+        form = QPDNDProjectForm(request=self.request)
+        self.assertFalse(form.is_valid())
+
+        # Test Create
+        # -----------
+        form_data = {
+            'project': self.project.instance,
+            'endpoint': 'point',
+            'version': '1.0.0',
+            'terms_of_service': 'https://smartbear.com/terms-of-use/',
+            'contact_author': 'Walter Lorenzetti',
+            'contact_email': 'lorenzetti@gis3w.it',
+            'contact_url': 'https://gis3w.it',
+            'title': 'Title of service',
+            'x_summary': 'Brief description',
+            'license': '1'
+
+        }
+
+        form = QPDNDProjectForm(request=self.request, data=form_data)
+        self.assertTrue(form.is_valid())
+        form.save()
+
+        iu_proj = QPDNDProject.objects.get(project=self.project.instance)
+
+        # Test Update
+        # -----------
+
+        initial_form_data = copy.copy(form_data)
+
+        form_data.update({
+            'note': 'note test'
+        })
+
+        form = QPDNDProjectForm(request=self.request, data=form_data, instance=iu_proj, initial=initial_form_data)
+        self.assertTrue(form.is_valid())
+        form.save()
+
+        iu_proj.refresh_from_db()
+        self.assertEqual(iu_proj.note, 'note test')
+
+        # Test NO WFS ACTIVATED
+        # ---------------------
+        form_data.update({
+            'project': self.project_no_wfs.instance
+        })
+
+        form = QPDNDProjectForm(request=self.request, data=form_data)
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['project'], ['The project must have almost one vector layer exposed as WFS service!'])
+
+
+
+
+
+
+
